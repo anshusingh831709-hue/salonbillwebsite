7f4bd06792664a0f16505fc0aeb3ab75b4d59221
ajit <ajits205055@gmail.com>
2026-05-10 13:18:31 +0530

    solution 1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'salon-billing-v1';
+const CACHE_NAME = 'salon-billing-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -13,6 +13,20 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
